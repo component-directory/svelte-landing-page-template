9d332b561144e9c097093f5d6c2136b96d96af63
unknown <component.directory@gmail.com>
2025-08-22 08:19:35 +0330

    Added meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import Hero from "$lib/components/home/hero.svelte";
-	import Features from "$lib/components/home/features.svelte";
-	import Faqs from "$lib/components/home/faqs.svelte";
-	import Metrics from "$lib/components/home/metrics.svelte";
-	import Testimonials from "$lib/components/home/testimonials.svelte";
-	import Technologies from "$lib/components/home/technologies.svelte";
+	import Hero from '$lib/components/home/hero.svelte';
+	import Features from '$lib/components/home/features.svelte';
+	import Faqs from '$lib/components/home/faqs.svelte';
+	import Metrics from '$lib/components/home/metrics.svelte';
+	import Testimonials from '$lib/components/home/testimonials.svelte';
+	import Technologies from '$lib/components/home/technologies.svelte';
 </script>
+
+<svelte:head>
+	<title>Free Full-Stack Svelte Landing Page Template – Open Source</title>
+	<meta
+		name="description"
+		content="Download a fully responsive, open-source landing page template built with Svelte 5 + ShadCN. Works seamlessly with full-stack apps, SaaS projects, and demos."
+	/>
+</svelte:head>
 
 <Hero />
 <Features />
